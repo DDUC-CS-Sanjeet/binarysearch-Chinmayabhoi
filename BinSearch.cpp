37ee183dde9ce01d74b19ebc17ec39b59074ba5f
Chinmayabhoi <59726688+Chinmayabhoi@users.noreply.github.com>
2020-01-17 22:56:25 +0530

    my second program

--- BinSearch.cpp
+++ BinSearch.cpp
@@ -1,20 +1,84 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 bool binarySearchReacursive(int* array, int startIndex, int lastIndex, int element)
 {
-  return true;
+	 int mid= (startIndex+lastIndex)/2;
+    
+       if(array[mid]==element)
+	 	 return true;
+	  else if(array[mid]>element)
+	  {
+        binarySearchReacursive(array,startIndex,mid,element);
+       }
+	   else
+	    {
+        binarySearchReacursive(array,mid,lastIndex,element);
+		}
+	return false;
 }
-
 bool binarySearchIterative(int* array, int startIndex, int lastIndex, int element)
-{
-  return true;
+
+{	
+	int middle = (startIndex+lastIndex)/2;
+	while (true)
+	{
+		if(array[middle] < element)
+		{
+			startIndex = middle;
+
+		}
+		else if(array[middle] == element)
+		{
+			return true;
+			break;
+		}
+		else
+		{
+			 lastIndex = middle ;
+		}
+	}
+  return false;
 }
 
 int main()
 {
-
-
-
-  return 0;
+	int n,element;
+	cout<<"\nEnter the size of array:";
+	cin>>n;
+	int array[n];
+	cout<<"\nEnter the element of array:";
+	for(int i=0;i<n;i++)
+	{
+		cin>>array[i];
+	}
+	sort(array,array+n);
+	cout<<"\nEnter the element to be searched:";
+	cin>>element;
+	int startIndex=0;
+	int lastIndex=n;
+	int x;
+	char ch;
+	do{
+		cout<<"Enter the way you want to do binary search:"<<endl;
+		cout<<"1.Recursion"<<endl;
+		cout<<"2.Iteration"<<endl;
+		cin>>x;
+		
+		switch(x){
+		
+		
+			case 1: cout<<binarySearchReacursive(array,startIndex,lastIndex,element);
+					break;
+			case 2:	 cout<<binarySearchIterative(array,startIndex,lastIndex,element);	
+	                break;
+	                
+	        default :
+	        	cout<<"invalid choice";
+				}
+		cout<<"\n want to enter more(y/n) :";
+		cin>>ch;
+  		}		while(ch!='n');
+	return 0;
 }
